3103650ef02973285b7fd040d14a4487ed040414
Endalkachew Bogale Gelaw <egelaw@stevens.edu>
2024-05-05 19:58:28 -0400

    Final checkes on the RecommenderGUI

--- RecommenderGUI.py
+++ RecommenderGUI.py
@@ -12,14 +12,12 @@ from Recommender import Recommender
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-
 class RecommenderGUI:
     def __init__(self):
-        # Creates an instance of a Recommender object and stores it in a variable
+        # Creates an instance of a Recommender object
         self.__recommender = Recommender()
 
-        # Creates a Toplevel main window, with an appropriate title, and dimensions of 1200
-        # pixels wide by 800 pixels tall
+        # Creates main window
         self.__main_window = tkinter.Tk()
         self.__main_window.title("Media Recommender")
         self.__main_window.geometry("1200x800")
@@ -46,15 +44,14 @@ class RecommenderGUI:
         self.quit_button = tkinter.Button(self.__buttons_frame, text='Quit', command=self.__main_window.quit)
         self.quit_button.pack(side='left', padx=75)
 
-        # pack notebook after buttons are packed
+        # pack notebook
         self.__notebook.pack(expand=True, fill='both')
-
 
         # Movies tab
         self.__movie_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__movie_tab, text='Movies')
 
-        # Frame for the movie_text widget and its scrollbar
+        # Frame for the movie_text widget
         self.__movie_frame = tkinter.Frame(self.__movie_tab)
 
         # Frame for the movie_stats
@@ -66,11 +63,12 @@ class RecommenderGUI:
 
         # Text widget to display the movie
         self.__movie_text = tkinter.Text(self.__movie_frame, wrap='word')
+
         # Default text to display
         self.__movie_text.insert(tkinter.END, "No movies have been loaded yet.")
         self.__movie_text.config(state='disabled')
 
-        # a vertical scrollbar
+        # Vertical scrollbar
         self.__scrollbar = tkinter.Scrollbar(self.__movie_frame, orient='vertical', command=self.__movie_text.yview)
         self.__scrollbar.pack(side='right', fill='y')
 
@@ -87,23 +85,24 @@ class RecommenderGUI:
         self.__TV_show_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__TV_show_tab, text='TV Shows')
 
-        # Frame for the TV_show_text widget and its scrollbar
+        # Frame for the TV_show_text widget
         self.__TV_show_frame = tkinter.Frame(self.__TV_show_tab)
 
         # Frame for the TV_show_stats
         self.__TV_show_stats_frame = tkinter.Frame(self.__TV_show_tab)
 
-        # pack frames
+        # Pack frames
         self.__TV_show_frame.pack(expand=True, fill='both')
         self.__TV_show_stats_frame.pack(expand=True, fill='both')
 
         # Text widget to display the TV_show
         self.__TV_show_text = tkinter.Text(self.__TV_show_frame, wrap='word')
+
         # Default text to display
         self.__TV_show_text.insert(tkinter.END, "No TV_shows have been loaded yet.")
         self.__TV_show_text.config(state='disabled')
 
-        # a vertical scrollbar
+        # Vertical scrollbar
         self.__scrollbar = tkinter.Scrollbar(self.__TV_show_frame, orient='vertical', command=self.__TV_show_text.yview)
         self.__scrollbar.pack(side='right', fill='y')
 
@@ -120,23 +119,24 @@ class RecommenderGUI:
         self.__book_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__book_tab, text='Books')
 
-        # Frame for the book_text widget and its scrollbar
+        # Frame for the book_text widget
         self.__book_frame = tkinter.Frame(self.__book_tab)
 
         # Frame for the book_stats
         self.__book_stats_frame = tkinter.Frame(self.__book_tab)
 
-        # pack frames
+        # Pack frames
         self.__book_frame.pack(expand=True, fill='both')
         self.__book_stats_frame.pack(expand=True, fill='both')
 
         # Text widget to display the books
         self.__book_text = tkinter.Text(self.__book_frame, wrap='word')
+
         # Default text to display
         self.__book_text.insert(tkinter.END, "No books have been loaded yet.")
         self.__book_text.config(state='disabled')
 
-        # a vertical scrollbar
+        # Vertical scrollbar
         self.__scrollbar = tkinter.Scrollbar(self.__book_frame, orient='vertical', command=self.__book_text.yview)
         self.__scrollbar.pack(side='right', fill='y')
 
@@ -160,8 +160,7 @@ class RecommenderGUI:
 
         self.__search_type_var = tkinter.StringVar()
         self.__search_type_var.set("Movie")
-        self.__search_type_combobox = ttk.Combobox(self.__show_type_frame, textvariable=self.__search_type_var,
-                                                 values=["Movie", "TV Show"],)
+        self.__search_type_combobox = ttk.Combobox(self.__show_type_frame, textvariable=self.__search_type_var, values=["Movie", "TV Show"],)
         self.__search_type_combobox.pack(side='left')
 
         # Title, Director, Actor and Genre frames
@@ -212,8 +211,6 @@ class RecommenderGUI:
         self.__search_show_scrollbar = tkinter.Scrollbar(self.__show_search_results_text, orient='vertical', command=self.__show_search_results_text.yview)
         self.__search_show_scrollbar.pack(side='right', fill='y')
 
-
-        # =======================BOOK STARTS======================================================================
         # Search Books
         self.__search_book_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__search_book_tab, text='Search Books')
@@ -255,17 +252,14 @@ class RecommenderGUI:
         self.__book_search_results_text.config(state='disabled')
 
         # a vertical scrollbar
-        self.__search_book_scrollbar = tkinter.Scrollbar(self.__book_search_results_text, orient='vertical', 
-                                                         command=self.__book_search_results_text.yview)
+        self.__search_book_scrollbar = tkinter.Scrollbar(self.__book_search_results_text, orient='vertical', command=self.__book_search_results_text.yview)
         self.__search_book_scrollbar.pack(side='right', fill='y')
-    # =========================BOOK ENDS===================================================================
 
-    # +++++++++++++++++++++++++++RECOMMENDATION STARTS++++++++++++++++++++++++++++++++++++++++++++++++++++++
         # Media Recommendation tab
         self.__recom_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__recom_tab, text="Recommendations")
         
-        # A Combobox with the options Movie, TV Show, Book
+        # Combobox with the options Movie, TV Show, Book
         self.__recom_type_frame = tkinter.Frame(self.__recom_tab)
         self.__recom_type_frame.pack(fill='both')
         self.__recom_type_label = tkinter.Label(self.__recom_type_frame, text="Type:")
@@ -275,7 +269,7 @@ class RecommenderGUI:
         self.__recom_type_combobox = ttk.Combobox(self.__recom_type_frame, textvariable=self.__recom_type_var, values=["Movie", "TV Show", "Book"])
         self.__recom_type_combobox.pack(side='left')
 
-        # Appropriate Label and Entry widgets to collection information regarding the title
+        # Appropriate Label and Entry widgets for title
         self.__recom_title_frame = tkinter.Frame(self.__recom_tab)
         self.__recom_title_frame.pack(fill='both')
         self.__recom_title_label = tkinter.Label(self.__recom_title_frame, text="Title:")
@@ -283,33 +277,29 @@ class RecommenderGUI:
         self.__recom_title_entry = tkinter.Entry(self.__recom_title_frame, width=70)
         self.__recom_title_entry.pack(side='left', fill='x')
 
-        # A Button to trigger the recommendation search, which calls the appropriate function from the Recommender object and stores the results in the text area
+        # Button for the recommendation search
         self.__recom_search_button = tkinter.Button(self.__recom_tab, text="Get Recommendation", command=self.get_recommendations)
         self.__recom_search_button.pack(anchor='w')
 
-        # If no searches have been performed yet, the text areas should display some default text to inform the user they need to enter a title to receive a recommendation
+        # Default text
         self.__recom_text = tkinter.Text(self.__recom_tab, wrap='word')
         self.__recom_text.pack(fill='both', expand=True, padx=5, pady=5)
         self.__recom_text.insert(tkinter.END, "Enter a title to receive a recommendation.")
         self.__recom_text.config(state='disabled')
 
-        # The user should be able to scroll through the results if they are too long to fit in the text area
+        # Scroll bar
         self.__recom_scrollbar = tkinter.Scrollbar(self.__recom_text, orient='vertical', command=self.__recom_text.yview)
         self.__recom_scrollbar.pack(side='right', fill='y')   
-    # +++++++++++++++++++++++++++RECOMMENDATION ENDS++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    # =======================BONUS RATING STARTS======================================================================
         # Ratings tab
         self.__rating_tab = tkinter.Frame(self.__notebook)
         self.__notebook.add(self.__rating_tab, text='Ratings')
 
-        # Add these lines to keep a reference to the canvas widgets
+        # Reference to the canvas widgets
         self.__movie_canvas = None
         self.__tv_canvas = None
 
-
-    # =========================BONUS RATING ENDS===================================================================
-
+    # Define the load_shows method
     def load_shows(self):
 
         # Clear old figures
@@ -345,17 +335,16 @@ class RecommenderGUI:
         movie_fig, ax = plt.subplots()
         ax.pie(list(movie_rating_dict.values()), labels=list(movie_rating_dict.keys()),
                autopct='%1.0f%%', startangle=90, colors=plt.cm.viridis(np.linspace(0, 1, len(movie_rating_dict))))
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        ax.set_title("Movie Ratings")  # Add a title
-        ax.legend(movie_rating_dict.keys(), title="Ratings", loc="upper right")  # Add a legend
+        ax.axis('equal')
+        ax.set_title("Movie Ratings")
+        ax.legend(movie_rating_dict.keys(), title="Ratings", loc="upper right")
 
         tv_fig, ax = plt.subplots()
         ax.pie(list(tv_rating_dict.values()), labels=list(tv_rating_dict.keys()),
                autopct='%1.0f%%', startangle=90, colors=plt.cm.viridis(np.linspace(0, 1, len(tv_rating_dict))))
-        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-        ax.set_title("TV Show Ratings")  # Add a title
-        ax.legend(tv_rating_dict.keys(), title="Ratings", loc="upper right")  # Add a legend
-
+        ax.axis('equal')
+        ax.set_title("TV Show Ratings")
+        ax.legend(tv_rating_dict.keys(), title="Ratings", loc="upper right")
 
         # Create a FigureCanvasTkAgg object for each figure
         self.__movie_canvas = FigureCanvasTkAgg(movie_fig, master=self.__rating_tab)
@@ -368,10 +357,11 @@ class RecommenderGUI:
         self.__tv_canvas.draw()
         self.__tv_canvas.get_tk_widget().pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
 
+    # Define the load_associations method
     def load_associations(self):
         self.__recommender.load_associations()
-    # ========================================================
-    # Define the load_books method for the Load Books button
+    
+    # Define the load_books method
     def load_books(self):
         self.__recommender.load_books()
         books = self.__recommender.get_book_list()
@@ -381,23 +371,17 @@ class RecommenderGUI:
         self.__book_text.delete(1.0, 'end')
         self.__book_stats_text.delete(1.0, 'end')
         try:
-            # After loading the books, insert them into the Text widget
             self.__book_text.insert('end', books)
         except tkinter.TclError:  # In case the user closes the window
-        # except Exception:   # This also works, but it is not recommended
             # In case nothing is passed (e.g., loading is canceled)
             return  # Do nothing
         self.__book_stats_text.insert('end', stats)
         self.__book_text.config(state='disabled')
         self.__book_stats_text.config(state='disabled')
-
-    # ========================================================
     
     # Show information about team members and project completion date
     def credit_info_box(self):
-        # Group members
         group_members = ["Ismail Gul", "Enalkachew Gelaw"]
-        # Day the project was completed on
         completion_day = "May 7th, 2024"
         
         # Create the message
@@ -409,7 +393,7 @@ class RecommenderGUI:
         # Show messagebox
         tkinter.messagebox.showinfo("Credit Information", message)
 
-
+    # Define the search_shows method
     def search_shows(self):
         show_type = self.__search_type_combobox.get()
         show_title = self.__show_title_search_entry.get()
@@ -427,23 +411,23 @@ class RecommenderGUI:
         self.__show_search_results_text.insert("end", results)
         self.__show_search_results_text.config(state='disabled')
 
+    # Define the search_books method
     def search_books(self):
         book_title = self.__book_search_entry.get()
         book_author = self.__book_author_entry.get()
         book_publisher = self.__book_publisher_entry.get()
         results = self.__recommender.search_books(book_title, book_author, book_publisher)
 
-        # Clear the text widget before inserting new results
+        # Clear the text widget
         self.__book_search_results_text.config(state='normal')
         self.__book_search_results_text.delete(1.0, 'end')
 
-        # Insert the search results into the text widget
+        # Insert the search results
         self.__book_search_results_text.insert("end", results)
         self.__book_search_results_text.config(state='disabled')
         
     # Method to display recommendations in the text area
     def get_recommendations(self):
-        # Extract data from GUI elements
         type = self.__recom_type_var.get()
         title = self.__recom_title_entry.get()
         
